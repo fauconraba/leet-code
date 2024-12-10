a04273c366b95601fe5b2d51daeb722ce644c58a
haytam raba <124938747+fauconraba@users.noreply.github.com>
2024-12-10 23:00:27 +0100

    Create roman_to_integer.cpp

---
+++ roman_to_integer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> romanValues = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
+        int result = 0;
+        int prevValue = 0;
+        int i;
+    
+        for ( i = s.size() - 1; i >= 0; i--) {
+            int currentValue = romanValues[s[i]];
+            
+            
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            
+            prevValue = currentValue;
+        }
+        
+        return result;
+    
+        }
+
+    
+};
